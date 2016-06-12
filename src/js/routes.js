28c01887b63278317f59c03a79cdc808588dae59
Damien Lecan <dev@dlecan.com>
2016-06-12 22:55:28 +0200

    WIP on login view

--- src/js/routes.js
+++ src/js/routes.js
@@ -4,10 +4,12 @@ import { Route, IndexRoute, Redirect } from 'react-router';
 import App from './components/App';
 import HomeView from './views/HomeView';
 import NotFoundView from './views/NotFoundView';
+import LoginView from './views/LoginView';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={HomeView} />
+    <Route path="login" component={LoginView} />
     <Route path="404" component={NotFoundView} />
     <Redirect from="*" to="404" />
   </Route>
