de5e66cb485d5d8930692f73ca33510f763e46ad
Damien Lecan <dev@dlecan.com>
2016-09-12 22:17:43 +0200

    Remove useless double check

--- src/js/views/HomeView/index.js
+++ src/js/views/HomeView/index.js
@@ -21,18 +21,14 @@ export default class HomeView extends React.Component {
               ?
                 <div>
                   <h1>Bienvenue au Club !</h1>
-                  <p>TODO : si compte non complété, demander les infos manquantes</p>
+                  {this.props.children}
                 </div>
-              :  ""
-            }
-            {!isAuthenticated
-              ?
+              :
                 <Jumbotron>
                   <h1>Club Expressions</h1>
                   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus scelerisque aliquam ligula, dictum tristique ipsum suscipit ac. Sed sed neque a ligula placerat sagittis. Curabitur nec lacus vel dolor maximus convallis a et ligula. Phasellus ut lacus sem. Donec rhoncus vulputate mi a elementum. Donec varius lacus a maximus mattis. Vivamus vehicula vitae augue at bibendum.</p>
                   <p><Button bsStyle="primary">Découvrir</Button></p>
                 </Jumbotron>
-              :  ""
             }
           </div>
         );
